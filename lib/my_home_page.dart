52a8a99dfe6b9710d1ce920f3a60b0da22629fe9
Bruno Raitz Miola <113939280+breno-bruno@users.noreply.github.com>
2026-03-13 12:46:35 +0000

    dand sequencia na criacao do menu lateral

---
+++ lib/my_home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/menu_lateral.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+    
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        leading: Builder(builder: (context)=>IconButton(onPressed: () {
+          Scaffold.of(context).openDrawer()
+        }, icon: const Icon(Icons.menu))),
+      ),
+      drawer: const MenuLateral(),
+      body: const Center(
+        child: Text(
+          "Selecione uma aula no menu",
+          style:TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
